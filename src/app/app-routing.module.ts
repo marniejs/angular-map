7f1276fec321112da7b5fa2a35f136280d55ec45
marniejs <marniejstaples@gmail.com>
2023-07-24 10:39:26 -0700

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './components/map/map.component';
+import { CountryDataComponent } from './components/country-data/country-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'map-component',
+    component: MapComponent,
+    children: [
+      {
+        path: 'country-data-component',
+        component: CountryDataComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
